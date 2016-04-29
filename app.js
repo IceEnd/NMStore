fb381fc5f7d7ee6e39ad5188997a3c96a186e509
mindanfeng <mindanfeng@noah-fund.com>
2016-04-29 16:41:56 +0800

    ssss

--- app.js
+++ app.js
@@ -16,19 +16,22 @@ var storema = require('./routes/storema');
 var goods = require('./routes/goods');
 var shopping = require('./routes/shopping');
 var car = require('./routes/car');
+var ws = require('./routes/ws');
 
 var app = express();
-var accessLogStream = fs.createWriteStream('./logs/access.log', {flags: 'a'})
+
+var expressWs = require('express-ws')(app);
+var accessLogStream = fs.createWriteStream('./logs/access.log', { flags: 'a' })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-app.engine('.html',require('ejs').__express);
+app.engine('.html', require('ejs').__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(logger('combined', {stream: accessLogStream}));
+app.use(logger('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -36,19 +39,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(connect.session({
   secret: '123456',
-  key: '123456' ,
-  cookie: { maxAge: 8000000}
+  key: '123456',
+  cookie: { maxAge: 8000000 }
 }));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/storema',storema);
-app.use('/goods',goods);
-app.use('/shopping',shopping);
-app.use('/car',car);
+app.use('/storema', storema);
+app.use('/goods', goods);
+app.use('/shopping', shopping);
+app.use('/car', car);
+app.use('/ws',car);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -59,7 +63,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -70,7 +74,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- public/javascripts/goods.js
+++ public/javascripts/goods.js
@@ -1,88 +1,88 @@
 //清除cookie
-function clearCookie(array){
-    for(i in array){
+function clearCookie(array) {
+    for (i in array) {
         $.removeCookie(array[i]);
     }
 }
 
 //登出
-$('#logout').click(function(){
-    clearCookie(['user_id','username','user_type','store_id','store_name','car']); 
+$('#logout').click(function () {
+    clearCookie(['user_id', 'username', 'user_type', 'store_id', 'store_name', 'car']);
     window.location.reload();
 });
 
 //添加到购物车
 $('#car_btn').click(function () {
     //$.cookie('user_id',data.user_id, { expires: 7 ,path: "/"});
-    if($('#number').val() == '0'){
+    if ($('#number').val() == '0') {
         alert('请选择数目');
-        return ;
+        return;
     }
-    if($.cookie('username') && $($.cookie('user_id'))){
+    if ($.cookie('username') && $($.cookie('user_id'))) {
         //用户已经登陆,mysql购物车
-        var car ={};
-        if($.cookie('car')){
+        var car = {};
+        if ($.cookie('car')) {
             //存在cookie
             car = $.parseJSON($.cookie('car'));
-            if(car.hasOwnProperty($('#car_btn').attr('data-goods'))){
+            if (car.hasOwnProperty($('#car_btn').attr('data-goods'))) {
                 car[$('#car_btn').attr('data-goods')] += parseInt($('#number').val());
             }
-            else{
+            else {
                 car[[$('#car_btn').attr('data-goods')]] = parseInt($('#number').val());
             }
         }
-        else{
+        else {
             car[[$('#car_btn').attr('data-goods')]] = parseInt($('#number').val());
         }
         $.ajax({
-            type:'POST',
-            url:'/goods/addcar',
-            dataType:'json',
+            type: 'POST',
+            url: '/goods/addcar',
+            dataType: 'json',
             traditional: true,
-            data:{
-                "car":JSON.stringify(car)
+            data: {
+                "car": JSON.stringify(car)
             },
-            success:function (data){
-                if(data){
+            success: function (data) {
+                if (data) {
                     clearCookie(['car']);
                     $('#number').val(0);
                     alert('添加成功');
                 }
-                else{
+                else {
                     alert('网络繁忙，请稍后再试...');
                 }
             },
-            error: function(xhr, errorType, error) {
+            error: function (xhr, errorType, error) {
                 alert('网络繁忙，请稍后再试...');
             }
         });
     }
-    else{
+    else {
         //cookie存储
         var car;
-        if($.cookie('car')){
+        if ($.cookie('car')) {
             //若cookie存在
             car = $.parseJSON($.cookie('car'));
-            if(car.hasOwnProperty($('#car_btn').attr('data-goods'))){
+            if (car.hasOwnProperty($('#car_btn').attr('data-goods'))) {
                 car[$('#car_btn').attr('data-goods')] += parseInt($('#number').val());
                 var temp = JSON.stringify(car);
-                $.cookie('car',temp,{ expires: 7 ,path: "/"});
+                $.cookie('car', temp, { expires: 7, path: "/" });
                 alert('添加成功');
                 $('#number').val(0);
             }
-            else{
+            else {
                 car[[$('#car_btn').attr('data-goods')]] = parseInt($('#number').val());
                 var temp = JSON.stringify(car);
-                $.cookie('car',temp,{ expires: 7 ,path: "/"});
+                $.cookie('car', temp, { expires: 7, path: "/" });
                 alert('添加成功');
                 $('#number').val(0);
             }
         }
-        else{
+        else {
             //若cookie不存在
             car = '';
-            car += '{"'+$('#car_btn').attr('data-goods')+'":'+$('#number').val()+'}';
-            $.cookie('car',car,{ expires: 7 ,path: "/"});
+            car += '{"' + $('#car_btn').attr('data-goods') + '":' + $('#number').val() + '}';
+            $.cookie('car', car, { expires: 7, path: "/" });
             alert('添加成功');
             $('#number').val(0);
         }
@@ -91,12 +91,28 @@ $('#car_btn').click(function () {
 
 //立即购买
 $('#buy_btn').click(function () {
-    if($('#number').val() == 0){
+    if ($('#number').val() == 0) {
         alert('请选择商品数目');
-        return ;
+        return;
     }
-    
+
     var gid = $('#buy_btn').attr('data-goods');
     var num = $('#number').val();
-    window.location.href = '/shopping?gid='+gid+'&num='+num;
+    window.location.href = '/shopping?gid=' + gid + '&num=' + num;
+});
+
+//点击聊天按钮
+$('#talk-icon').bind('click', function () {
+    //验证浏览器是否支持
+    if (!window.WebSocket) {
+        alert("WebSocket not supported by this browser!");
+    }
+    
+    if ($.cookie('username') && $.cookie('user_id') && $.cookie('user_type')) {
+
+    }
+    else {
+        alert('请先登录');
+        return;
+    }
 });
